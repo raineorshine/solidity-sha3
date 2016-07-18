e492e99a100b0ab0edd18dc7cba3c1d720445f6b
Raine Revere <raineorshine@gmail.com>
2016-07-18 11:32:09 -0600

    Refactor sha3withsize.

--- src/index.js
+++ src/index.js
@@ -5,41 +5,34 @@ const web3 = new Web3()
 // the size of a character in a hex string in bytes
 const HEX_CHAR_SIZE = 4
 
+// the size to hash an integer if not explicity provided
+const DEFAULT_SIZE = 256
+
 const isHex = val => val.toString().slice(0, 2) === '0x'
 
-export default (...args) => {
-  const paddedArgs = args.map(arg => {
-    // non-hex string
-    if (typeof arg === 'string' && !isHex(arg)) {
-      return web3.toHex(arg)
-    }
-    // numbers, big numbers, and hex strings
-    else {
-      const argString = typeof arg === 'number'
-        ? arg.toString(16)
-        : web3.toHex(arg).slice(2)
-      return leftPad(argString, 64, 0)
-    }
-  }).join('')
+/** Encodes a values in hex and adds padding to the given size if needed. Curried args. */
+const encodeWithPadding = size => value => {
+  // non-hex string
+  if (typeof value === 'string' && !isHex(value)) {
+    return web3.toHex(value)
+  }
+  // numbers, big numbers, and hex strings
+  else {
+    const argString = typeof value === 'number'
+      ? value.toString(16)
+      : web3.toHex(value).slice(2)
+    return leftPad(argString, size / HEX_CHAR_SIZE, 0)
+  }
+}
 
+/** Hashes one or more arguments, using a default size for numbers. */
+export default (...args) => {
+  const paddedArgs = args.map(encodeWithPadding(DEFAULT_SIZE)).join('')
   return web3.sha3(paddedArgs, { encoding: 'hex' })
 }
 
+/** Hashes a single value at the given size. */
 export const sha3withsize = (value, size) => {
-
-  const paddedArgs = (arg => {
-    // non-hex string
-    if (typeof arg === 'string' && !isHex(arg)) {
-      return web3.toHex(arg)
-    }
-    // numbers, big numbers, and hex strings
-    else {
-      const argString = typeof arg === 'number'
-        ? arg.toString(16)
-        : web3.toHex(arg).slice(2)
-      return leftPad(argString, size / HEX_CHAR_SIZE, 0)
-    }
-  })(value)
-
+  const paddedArgs = encodeWithPadding(size)(value)
   return web3.sha3(paddedArgs, { encoding: 'hex' })
 }
